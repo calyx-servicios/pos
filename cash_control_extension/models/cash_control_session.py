4987f3fa0b18b5584de875c1b43f7036958b6396
marcooegg <marco.oegg@gmail.com>
2022-05-10 09:30:05 -0300

    [UPD][cash_control_extension] - format changes

--- cash_control_extension/models/cash_control_session.py
+++ cash_control_extension/models/cash_control_session.py
@@ -8,21 +8,41 @@ _logger = logging.getLogger(__name__)
 class CashControlSession(models.Model):
     _inherit = "cash.control.session"
 
-    user_id = fields.Many2one(comodel_name='res.users', string='Responsible')
-    user_ids = fields.Many2many(comodel_name='res.users', string='Assignees')
+    user_id = fields.Many2one(comodel_name="res.users", string="Responsible")
+    user_ids = fields.Many2many(comodel_name="res.users", string="Assignees")
 
+    transfer_ids = fields.Many2many(
+        string="transfers",
+        comodel_name="account.bank.statement.line",
+        compute="_compute_transfer_ids",
+    )
+    invoice_ids = fields.Many2many(
+        string="Invoices",
+        comodel_name="account.move",
+        compute="_compute_invoice_ids",
+    )
 
-    transfer_ids = fields.Many2many(string="transfers", comodel_name='account.bank.statement.line', compute="_compute_transfer_ids")
+    def _compute_invoice_ids(self):
+        for session in self:
+            session.invoice_ids = self.env["account.move"].search(
+                [
+                    ("cash_control_session_id", "=", session.id),
+                    ("journal_id.type", "in", ["sale", "purchase"]),
+                ]
+            )
 
     @api.depends("statement_id")
     def _compute_transfer_ids(self):
         for session in self:
             session.transfer_ids = self.env["account.bank.statement.line"].search(
-                [("statement_id", "=", session.statement_id.id),("transaction_type",'in',['TRANSFER_OUT','TRANSFER_IN'])]
+                [
+                    ("statement_id", "=", session.statement_id.id),
+                    ("transaction_type", "in", ["TRANSFER_OUT", "TRANSFER_IN"]),
+                ]
             )
 
     @api.model
-    def create(self,vals):
+    def create(self, vals):
         res = super().create(vals)
         try:
             res.user_id = self.env.user.id
