353a4697ed90c2ea1af7f933d1a9405fa5c6b774
gabbii <gperez@calyxservicios.com.ar>
2022-05-11 17:00:00 -0300

    [FIX][pos_promotion_season_brand]ODOO-3841. - FIX name fields.

--- pos_promotion_season_brand/models/pos_promotion_brand.py
+++ pos_promotion_season_brand/models/pos_promotion_brand.py
@@ -3,15 +3,15 @@ from odoo import fields, models
 class PosPromotionBrand(models.Model):
     _name = 'pos.promotion.brand'
 
-    promotion_id = fields.Many2one("Promotion", 
-        comodel_name="pos.promotion")
-    promotion_code = fields.Char("Promotion Code",
-                                 related='promotion_id.promotion_code',
+    promotion_id = fields.Many2one( 
+        comodel_name="pos.promotion", "Promotion")
+    promotion_code = fields.Char(related='promotion_id.promotion_code',
+                                 "Promotion Code",
                                  store=True)
 
-    state = fields.Selection('State',
-        related='promotion_id.state', store=True)
-    brand_id = fields.Many2one('Brand', 'product.brand', required=True)
+    state = fields.Selection(
+        related='promotion_id.state', 'State', store=True)
+    brand_id = fields.Many2one('product.brand', 'Brand', required=True)
     fixed_price = fields.Float('Fixed Price')
     disc_percentage = fields.Float('Disc. %')
     disc_amount = fields.Float('Disc. Amount')
--- pos_promotion_season_brand/models/pos_promotion_season.py
+++ pos_promotion_season_brand/models/pos_promotion_season.py
@@ -3,15 +3,15 @@ from odoo import fields, models
 class PosPromotionSeason(models.Model):
     _name = 'pos.promotion.season'
 
-    promotion_id = fields.Many2one("Promotion",
-        comodel_name="pos.promotion")
-    promotion_code = fields.Char("Promotion Code",
-                                 related='promotion_id.promotion_code',
+    promotion_id = fields.Many2one(
+        comodel_name="pos.promotion", "Promotion")
+    promotion_code = fields.Char(related='promotion_id.promotion_code',
+                                 "Promotion Code",
                                  store=True)
 
-    state = fields.Selection('State',
-        related='promotion_id.state', store=True)
-    season_id = fields.Many2one('Season', 'product.seasons', required=True)
+    state = fields.Selection(
+        related='promotion_id.state', 'State', store=True)
+    season_id = fields.Many2one('product.seasons', 'Season', required=True)
     fixed_price = fields.Float('Fixed Price')
     disc_percentage = fields.Float('Disc. %')
     disc_amount = fields.Float('Disc. Amount')
