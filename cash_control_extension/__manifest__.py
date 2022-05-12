4591b6e4855790e1538e09d139f34b29ae022df4
marcooegg <marco.oegg@gmail.com>
2022-05-12 09:36:24 -0300

    [UPD][cash_control_extension] - add session_date to sale.report - translations - add doc/changelog.rst - upd manifest

--- cash_control_extension/__manifest__.py
+++ cash_control_extension/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Endpoint",
-    "version": "13.0.0.0.1",
+    "version": "13.0.1.0.1",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
--- cash_control_extension/models/__init__.py
+++ cash_control_extension/models/__init__.py
@@ -1,3 +1,4 @@
 from . import cash_control_config
 from . import cash_control_session
 from . import sale_order
+from . import sale_report
