d0f891c41b2db8d7d0e6016fd5eeab3122c57555
Marco Oegg <marco.oegg@gmail.com>
2022-11-11 21:13:24 -0300

    [UPD][cash_control_multi_store] - changed how store is assigned in account_move

--- cash_control_multi_store/models/__init__.py
+++ cash_control_multi_store/models/__init__.py
@@ -1,3 +1,4 @@
 from . import account_journal
 from . import cash_control_config
 from . import sale_order
+from . import account_move
---
+++ cash_control_multi_store/models/account_move.py
@@ -0,0 +1,28 @@
+from odoo import models,fields,api,_
+from odoo.exceptions import UserError
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    @api.model
+    def _default_store_id(self):
+        return self.journal_id.store_id.id
+
+    store_id = fields.Many2one(comodel_name="res.store",default=_default_store_id)
+
+    @api.onchange("cash_control_session_id","journal_id")
+    def _onchange_cash_control_session_id(self):
+        if self.cash_control_session_id: #we prioritize cash control store
+            self.store_id = self.cash_control_session_id.config_id.store_id.id
+        else:
+            self.store_id = self._default_store_id()
+
+    def post(self):
+        if not self.journal_id.multi_store:
+            if not self.store_id:
+                store_id = self.journal_id.id
+                if not store_id:
+                    raise UserError(_("Move must have a store assigned if journal is not Multi Store"))
+                self.store_id = store_id.id
+        return super().post()
