a706ab04b979b6f83eb0c717f6cb0ccf8ae0cb42
Marco Oegg <marco.oegg@gmail.com>
2023-01-13 11:44:15 -0300

    [UPD][cash_control,cash_control_multi_store] - add multi-company security rules - update _search methods - ensure store_id.company_id and config_id.company_id are the same

--- cash_control_multi_store/models/cash_control_session.py
+++ cash_control_multi_store/models/cash_control_session.py
@@ -9,13 +9,11 @@ class CashControlSession(models.Model):
     @api.model
     def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
         """
-        Para que usuarios los usuarios no puedan elegir diarios donde no puedan
-        escribir, modificamos la funcion search. No lo hacemos por regla de
-        permiso ya que si no pueden ver los diarios termina dando errores en
-        cualquier lugar que se use un campo related a algo del diario
+        Same restriction applied to models in multi-store modules. We add user.store_ids to allow multiple
+        stores' configs to be shown at once
         """
         user = self.env.user
         # if superadmin, do not apply
         if not self.env.is_superuser():
-            args += ['|', ('store_id', '=', False), ('store_id', 'child_of', [user.store_id.id])]
+            args += ['|','|',('store_id', '=', False), ('store_id', 'child_of', [user.store_id.id]),("store_id","in",user.store_ids.ids)]
         return super()._search(args, offset, limit, order, count=count, access_rights_uid=access_rights_uid)
