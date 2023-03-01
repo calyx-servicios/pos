8adf409ace5ec6614ba5751c3c449040caa864e9
Marco Oegg <marco.oegg@gmail.com>
2023-03-01 09:05:59 -0300

    [FCO-348][ADD][cash_control_analytic_account] - add module

--- cash_control_analytic_account/models/account_move_line.py
+++ cash_control_analytic_account/models/account_move_line.py
@@ -9,19 +9,4 @@ class AccountMoveLine(models.Model):
         string="Analytic Account",
         index=True,
         related="move_id.cash_control_session_id.config_id.analytic_account_id",
-        # compute="_compute_analytic_account_id",
-        # inverse="_inverse_analytic_account_id",
     )
-
-
-    @api.depends("move_id.cash_control_session_id")
-    def _compute_analytic_account_id(self):
-        for line in self:
-            if line.move_id.cash_control_session_id:
-                line.analytic_account_id = line.move_id.cash_control_id.config_id.analytic_account_id.id
-            else:
-                line.analytic_account_id = False
-    
-    def _inverse_analytic_account_id(self):
-        for line in self:
-            print("how the fuck does inverse work")
--- cash_control_analytic_account/models/account_payment.py
+++ cash_control_analytic_account/models/account_payment.py
@@ -1,110 +1,15 @@
 from odoo import models, fields, api, _
 
-import logging
-
-_logger = logging.getLogger("ACCOUNT PAYMENT DICE QUE:")
-
 
 class AccountPayment(models.Model):
     _inherit = "account.payment"
 
     def _prepare_payment_moves(self):
-        # this is here just to not break inheritance
-        all_move_vals = super()._prepare_payment_moves()
-        _logger.info(all_move_vals)
         all_move_vals = []
         for payment in self:
             move_vals = super(AccountPayment, payment)._prepare_payment_moves()
-            _logger.info(move_vals)
             for move in move_vals:
                 if payment.cash_control_session_id:
-                    _logger.info("se rompe en la asignacion")
                     move["cash_control_session_id"] = payment.cash_control_session_id.id
-                _logger.info("se rompe en el +=")
                 all_move_vals += [move]
-        _logger.info(all_move_vals)
         return all_move_vals
-
-    # TODO:
-    # FALTA TESTEAR ESTO^!
-
-
-# [
-#     {
-#         "date": datetime.date(2023, 3, 1),
-#         "ref": "1100501",
-#         "journal_id": 37,
-#         "currency_id": 19,
-#         "partner_id": False,
-#         "line_ids": [
-#             (
-#                 0,
-#                 0,
-#                 {
-#                     "name": "TRANS/2023/0623",
-#                     "amount_currency": 0.0,
-#                     "currency_id": False,
-#                     "debit": 12000.0,
-#                     "credit": 0.0,
-#                     "date_maturity": datetime.date(2023, 3, 1),
-#                     "partner_id": False,
-#                     "account_id": 1,
-#                     "payment_id": 1163,
-#                 },
-#             ),
-#             (
-#                 0,
-#                 0,
-#                 {
-#                     "name": "Transfer to Efectivo",
-#                     "amount_currency": 0.0,
-#                     "currency_id": False,
-#                     "debit": 0.0,
-#                     "credit": 12000.0,
-#                     "date_maturity": datetime.date(2023, 3, 1),
-#                     "partner_id": False,
-#                     "account_id": 910,
-#                     "payment_id": 1163,
-#                 },
-#             ),
-#         ],
-#     },
-#     {
-#         "date": datetime.date(2023, 3, 1),
-#         "ref": "1100501",
-#         "partner_id": False,
-#         "journal_id": 8,
-#         "line_ids": [
-#             (
-#                 0,
-#                 0,
-#                 {
-#                     "name": "TRANS/2023/0623",
-#                     "amount_currency": 0.0,
-#                     "currency_id": False,
-#                     "debit": 0.0,
-#                     "credit": 12000.0,
-#                     "date_maturity": datetime.date(2023, 3, 1),
-#                     "partner_id": False,
-#                     "account_id": 1,
-#                     "payment_id": 1163,
-#                 },
-#             ),
-#             (
-#                 0,
-#                 0,
-#                 {
-#                     "name": "Transfer from Visa Debito",
-#                     "amount_currency": 0.0,
-#                     "currency_id": False,
-#                     "debit": 12000.0,
-#                     "credit": 0.0,
-#                     "date_maturity": datetime.date(2023, 3, 1),
-#                     "partner_id": False,
-#                     "account_id": 299,
-#                     "payment_id": 1163,
-#                 },
-#             ),
-#         ],
-#     },
-# ]
