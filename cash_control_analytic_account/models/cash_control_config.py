971aff30001c0339252ba2082b869500932a192b
Marco Oegg <marco.oegg@gmail.com>
2023-03-01 08:57:45 -0300

    [ADD][cash_control_analytic_account] - add module

---
+++ cash_control_analytic_account/models/cash_control_config.py
@@ -0,0 +1,7 @@
+from odoo import models,fields,api,_
+
+class CashControlConfig(models.Model):
+    _inherit ="cash.control.config"
+
+    analytic_account_id = fields.Many2one(comodel_name="account.analytic.account")
+    
