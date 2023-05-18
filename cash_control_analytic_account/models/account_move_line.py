e23e647844a4365ea3c469ad2f0dc76d86d664c2
Marco Oegg <marco.oegg@gmail.com>
2023-05-18 15:49:24 -0300

    [ODOO-6668][UPD][cash_control_analytic_account] - workwarround so account_move_line analytic_account_id is editable

--- cash_control_analytic_account/models/account_move_line.py
+++ cash_control_analytic_account/models/account_move_line.py
@@ -1,12 +1,32 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
+    manual_analytic_account_id = fields.Many2one(
+        comodel_name="account.analytic.account",
+    )
+
     analytic_account_id = fields.Many2one(
         comodel_name="account.analytic.account",
         string="Analytic Account",
         index=True,
-        related="move_id.cash_control_session_id.config_id.analytic_account_id",
+        compute="_compute_analytic_account_id",
+        inverse="_inverse_analytic_account_id",
     )
+
+    @api.depends("move_id.cash_control_session_id", "manual_analytic_account_id")
+    def _compute_analytic_account_id(self):
+        for line in self:
+            cc_session_id = line.move_id.cash_control_session_id
+            if cc_session_id:
+                cc_analytic_account_id = cc_session_id.config_id.analytic_account_id
+                if cc_analytic_account_id:
+                    line.analytic_account_id = cc_analytic_account_id.id
+            else:
+                line.analytic_account_id = line.manual_analytic_account_id or False
+
+    def _inverse_analytic_account_id(self):
+        for line in self:
+            line.manual_analytic_account_id = line.analytic_account_id.id
