deaca9625f14984932ce74f0f3bacb5326de1788
gabbiiperez <gabbiiperez94@gmail.com>
2023-07-26 16:51:27 -0300

    [ODOO-7008] Update submodules

---
+++ cash_control_analytic_account/models/account_payment.py
@@ -0,0 +1,14 @@
+from odoo import models
+
+class AccountPayment(models.Model):
+    _inherit = "account.payment"
+
+    def _prepare_payment_moves(self):
+        all_move_vals = []
+        for payment in self:
+            move_vals = super(AccountPayment, payment)._prepare_payment_moves()
+            for move in move_vals:
+                if payment.cash_control_session_id:
+                    move["cash_control_session_id"] = payment.cash_control_session_id.id
+                all_move_vals += [move]
+        return all_move_vals
