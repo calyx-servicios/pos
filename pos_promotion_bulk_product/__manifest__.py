e3089685d61c9a4898b523da529f5c21035851c6
gabbiiperez <gabbiiperez94@gmail.com>
2023-07-28 13:08:23 -0300

    [ODOO-6866][IMP][pos_promotion_bulk_product] - IMP promotion bulk products.

--- pos_promotion_bulk_product/__manifest__.py
+++ pos_promotion_bulk_product/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "http://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Point of sale",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "installable": True,
     "application": False,
     "depends": [
