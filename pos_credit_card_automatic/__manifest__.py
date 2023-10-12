f5beb7ac07d37f8e2700b62784529414651c83d6
JaviZamoraDev <javier.zamora.dev@gmail.com>
2023-10-12 08:23:21 -0300

    [ODOO-7154][FIX][pos_credit_car_automatic] - Fix undefined translations var

--- pos_credit_card_automatic/__manifest__.py
+++ pos_credit_card_automatic/__manifest__.py
@@ -8,7 +8,7 @@
     "maintainers": ["PerezGabriela"],
     "website": "http://odoo.calyx-cloud.com.ar/",
     "category": "Point of sale",
-    "version": "13.0.1.1.0",
+    "version": "13.0.1.2.1",
     "installable": True,
     "application": False,
     "depends": [
--- pos_credit_card_automatic/static/src/js/credit_card_automatic.js
+++ pos_credit_card_automatic/static/src/js/credit_card_automatic.js
@@ -2,6 +2,7 @@ odoo.define('pos_credit_card_automatic.credit_card_automatic', function (require
 	"use strict";
 
 	let screens = require('point_of_sale.screens');
+	var _t = require('web.core')._t;
 
 	screens.PaymentScreenWidget.include({
 	    render_payment_terminal: function() {
@@ -10,13 +11,13 @@ odoo.define('pos_credit_card_automatic.credit_card_automatic', function (require
 			if (!order || !order.get_client()) {
 				this.pos.gui.show_popup('error', {
 					'title': _t('Error'),
-					'body': _t('Selecciona un cliente antes de continuar.'),
+					'body': _t('Please, select a customer before.'),
 				});
 				return;
 			}
-	    	
+
 	    	let paymentline = order.selected_paymentline;
-	    	
+
 	    	if (paymentline) {
 	    		let line = order.get_paymentline(paymentline.cid);
 	    		this.$el.find('.instalment').change(function(){
@@ -25,7 +26,7 @@ odoo.define('pos_credit_card_automatic.credit_card_automatic', function (require
 	        			payment_method['selected'] = $(this).val();
 	        		}
 	        	});
-	        	
+
 	        	this.$el.find('#btnData').click(function(){
 					var amount = line.get_amount();
 					let instalments = line.payment_method.instalments;
@@ -35,14 +36,14 @@ odoo.define('pos_credit_card_automatic.credit_card_automatic', function (require
 						let fee = amountCof - amount;
 						$("#selectPopupInstalments").append('<option value="' + instalments[obj]['id'] + '" coef="' + fee + '" amount="' + amountCof + '">' + instalments[obj]['name'] + '</option>');
 					};
-					
+
 					var payment_selected = self.$el.find('.instalment').val();
 					if (payment_selected) {
 						$('#selectPopupInstalments').val(payment_selected);
 					} else {
 						$('#selectPopupInstalments').val(1);
 					}
-					
+
 	        	    let fee = $('#selectPopupInstalments option:selected').attr('coef');
 	                let amountCof = $('#selectPopupInstalments option:selected').attr('amount');
 	        	    let instalment_id = $('#selectPopupInstalments option:selected').val();
@@ -54,14 +55,14 @@ odoo.define('pos_credit_card_automatic.credit_card_automatic', function (require
 	        	    paymentline['tiket_number'] = "1111";
 	        	    paymentline['lot_number'] = "1111";
 	        	    paymentline['fee'] = fee;
-	        	    
+
 	                order.add_product(product, {extras:{name: 'Cargo Tarjeta'}, price:fee,quantity:1, merge: false});
 	           	    paymentline.set_amount(amountCof);
 	        	    paymentline.set_payment_status('done');
 	        	    self.render_paymentlines();
 	        	    order.finalized = true;
 				});
-				
+
 	            if (line.payment_method.selected){
 	                let payment_selected = line.payment_method.selected;
 	            	self.$el.find('.instalment').val(payment_selected);
